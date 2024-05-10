da7384d1e5300c872745d2560b09651ab7729c9a
Bryan Jay <120381978+BryanJayHizole@users.noreply.github.com>
2024-05-10 12:45:45 +0800

    Update inventory.js

--- functions/routes/inventory.js
+++ functions/routes/inventory.js
@@ -6,7 +6,7 @@ const inventoryController = require('../controllers/inventoryController');
 // Define routes
 router.post('/inventory', inventoryController.addItem);
 router.get('/inventory', inventoryController.getAllItems);
-router.put('/inventory/:id', inventoryController.updateItem);
+router.put('/inventory/:itemToEdit.id', inventoryController.updateItem);
 router.delete('/inventory/:id', inventoryController.deleteItem);
 
 module.exports = router;
